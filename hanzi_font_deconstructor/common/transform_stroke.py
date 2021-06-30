0a66837315ef9fe297d2314347f0e5c2f5aa61bc
David Chanin <chanindav@gmail.com>
2021-06-30 16:56:53 +0100

    tweaking image generation logic

--- hanzi_font_deconstructor/common/transform_stroke.py
+++ hanzi_font_deconstructor/common/transform_stroke.py
@@ -9,17 +9,24 @@ def transform_stroke(stroke_pathstr: str, viewbox: Tuple[int, int, int, int]) ->
     vbMinX, vbMinY, vbWidth, vbHeight = viewbox
     vbMaxX = vbMinX + vbWidth
     vbMaxY = vbMinY + vbHeight
+    strokeMidX = (strokeMaxX + strokeMinX) / 2
+    strokeMidY = (strokeMaxY + strokeMinY) / 2
 
-    translateX = randint(int(vbMinX - strokeMinX), int(vbMaxX - strokeMaxX))
-    translateY = randint(int(vbMinY - strokeMinY), int(vbMaxY - strokeMaxY))
+
     rotate = uniform(-10, 10)
     skewX = uniform(-10, 10)
     skewY = uniform(-10, 10)
     scaleX = max(0.1, gauss(0.3, 0.2))
     scaleY = scaleX * uniform(0.9, 1.1)
 
+    # from https://stackoverflow.com/a/11671373
+    baseTranslateX = (1 - scaleX) * strokeMidX
+    baseTranslateY = (1 - scaleY) * strokeMidY
+    translateX = baseTranslateX + randint(int(vbMinX - strokeMinX), int(vbMaxX - strokeMaxX))
+    translateY = baseTranslateY + randint(int(vbMinY - strokeMinY), int(vbMaxY - strokeMaxY))
+
     return TransformedStroke(
-        translate=(translateX * min(scaleX, 1) * 0.9, translateY * min(scaleY, 1) * 0.9),
+        translate=(translateX, translateY),
         rotate=rotate,
         scale=(scaleX, scaleY),
         skewX=skewX,
