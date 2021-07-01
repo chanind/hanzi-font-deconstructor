3cd487404a5312c08748dba757c959f149b235c7
David Chanin <chanindav@gmail.com>
2021-07-01 17:48:51 +0100

    adding a helper to create a PIL image from a SVG

--- hanzi_font_deconstructor/common/generate_svg.py
+++ hanzi_font_deconstructor/common/generate_svg.py
@@ -9,26 +9,31 @@ class StrokeAttrs:
     transform: str
 
 
-def generate_svg(strokes_attrs: Sequence[StrokeAttrs], viewbox=Tuple[int, int, int, int]) -> str:
-    svg_paths=[]
+def generate_svg(
+    strokes_attrs: Sequence[StrokeAttrs], viewbox=Tuple[int, int, int, int]
+) -> str:
+    svg_paths = []
     for attrs in strokes_attrs:
-        svg_paths.append(f'<path d="{attrs.d}" transform={attrs.transform} />')
+        svg_paths.append(f'<path d="{attrs.d}" transform="{attrs.transform}" />')
     viewboxStr = f"{viewbox[0]} {viewbox[1]} {viewbox[2]} {viewbox[3]}"
     svgPathsStr = "\n".join(svg_paths)
     return (
         f'<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="{viewboxStr}">\n'
-        f'{svgPathsStr}\n'
-        '</svg>'
+        f"{svgPathsStr}\n"
+        "</svg>"
     )
+
 
 def get_stroke_attrs(stroke: TransformedStroke) -> StrokeAttrs:
     return StrokeAttrs(
         d=stroke.path,
         transform=(
-            f' translate({stroke.translate[0]}, {stroke.translate[1]})'
-            f' rotate({stroke.rotate})'
-            f' skewX({stroke.skewX})'
-            f' skewY({stroke.skewY})'
-            f' scale({stroke.scale[0]}, {stroke.scale[1]})'
-        )
+            " ".join([
+                f"translate({stroke.translate[0]}, {stroke.translate[1]})",
+                f"rotate({stroke.rotate})",
+                f"skewX({stroke.skewX})",
+                f"skewY({stroke.skewY})",
+                f"scale({stroke.scale[0]}, {stroke.scale[1]})",
+            ])
+        ),
     )
